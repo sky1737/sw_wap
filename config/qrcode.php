0d6cadf82a0c269835833f06474cf238f30457f3
dev <dev@waphe.com>
2016-06-12 14:09:15 +0800

    config

---
+++ config/qrcode.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * 配置文件
+ */
+if(!defined('TWIKER_PATH')) exit('deny access!');
+
+$cfg = array(
+	'bg' => '../static/qrcode/limit_bg.jpg',
+	'path' => '../upload/qrcode/' . floor($wap_user['uid'] / 1000) . '/' . floor($wap_user['uid'] / 100) . '/',
+	'filename' => $wap_user['uid'] . '.png',
+	'qrcode' => array('url' => '../static/qrcode/no.png', 'width' => 174 , 'x' => 132, 'y' => 706),
+	'avatar' => array('filename' => md5($wap_user['avatar']) . '.jpg', 'width' =>  61, 'x' => 209, 'y' => 34 ),
+	'nickname' => array('x' => 377, 'y' => 45),
+	'expire' => array('date' => '', 'x' => 170, 'y' => 900)
+);
