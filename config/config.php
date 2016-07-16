b3ca5dd10527eb758d9d5d78aa08d49bd3297d1d
谭康 <tkaven@vip.qq.com>
2016-07-16 17:20:56 +0800

    reset config

--- config/config.php
+++ config/config.php
@@ -4,20 +4,9 @@
  */
 if(!defined('TWIKER_PATH')) exit('deny access!');
 
-
-return array(
-	'DB_HOST'=>'127.0.0.1',
-	'DB_NAME'=>'yws_',//数据库名
-	'DB_USER'=>'root',//数据库用户名
-	'DB_PWD'=>'root',//数据库密码
-	'DB_PORT'=>'3306',
-	'DB_PREFIX'=>'tp_',
-	'DB_CHARSET'=>'utf8',
-);
-
 return array(
 	'DB_HOST'=>'rm-bp1m3x9u9vi8587ui.mysql.rds.aliyuncs.com',
-	'DB_NAME'=>'yws_',//数据库名
+	'DB_NAME'=>'yunws',//数据库名
 	'DB_USER'=>'yunws',//数据库用户名
 	'DB_PWD'=>'q1w2e3r4',//数据库密码
 	'DB_PORT'=>'3306',
