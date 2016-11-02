1fb20a957abc36e390dc1fba8d1b1b05daa75b2e
dev <dev@waphe.com>
2016-11-02 17:41:57 +0800

    众筹。

--- template/wap/default/drp_ucenter.php
+++ template/wap/default/drp_ucenter.php
@@ -108,7 +108,9 @@
         <li><a href="drp_products.php"><i class="icon-commission"></i><span class="text">商品库</span><i class="arrow"></i></a>
         </li>
         <li style="height: 5px;background: #eee;"></li>
-        <li class="last"><a href="app_z.php"><i class="icon-commission"></i><span class="text">云温商众筹</span><i
+        <li><a href="app_z.php"><i class="icon-commission"></i><span class="text">云温商众筹</span><i
+                    class="arrow"></i></a></li>
+        <li class="last"><a href="app_z_my.php"><i class="icon-commission"></i><span class="text">我的众筹</span><i
                     class="arrow"></i></a></li>
     </ul>
 </div>
