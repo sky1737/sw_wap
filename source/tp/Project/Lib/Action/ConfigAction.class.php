38989e61475016f1c9a3f82c84b182b886b4c972
dev <dev@waphe.com>
2016-11-02 21:19:36 +0800

    菜单管理.

--- source/tp/Project/Lib/Action/ConfigAction.class.php
+++ source/tp/Project/Lib/Action/ConfigAction.class.php
@@ -10,7 +10,7 @@
 
 class ConfigAction extends BaseAction
 {
-    public function index()
+	public function index()
     {
         $database_config_group = D('Config_group');
         $condition_config_group['status'] = '1';
@@ -35,7 +35,7 @@ class ConfigAction extends BaseAction
         $this->display();
     }
 
-    public function amend()
+	public function amend()
     {
         if (IS_POST) {
             $database_config = D('Config');
@@ -63,7 +63,7 @@ class ConfigAction extends BaseAction
         $this->display();
     }
 
-    protected function build_html($config_list)
+	protected function build_html($config_list)
     {
         if (is_array($config_list)) {
             $config_html = '';
@@ -147,117 +147,368 @@ class ConfigAction extends BaseAction
         }
     }
 
-    public function ajax_upload_pic()
-    {
-        if ($_FILES['imgFile']['error'] != 4) {
-            $img_admin_id = sprintf("%09d", $this->system_session['id']);
-            $rand_num = substr($img_admin_id, 0, 3) . '/' . substr($img_admin_id, 3, 3) . '/' . substr($img_admin_id, 6, 3);
-            $upload_dir = "./upload/images/{$rand_num}/";
-            if (!is_dir($upload_dir)) {
-                mkdir($upload_dir, 0777, true);
-            }
-            import('ORG.Net.UploadFile');
-            $upload = new UploadFile();
-            $upload->maxSize = 3 * 1024 * 1024;
-            $upload->allowExts = array('jpg', 'jpeg', 'png', 'gif');
-            $upload->allowTypes = array('image/png', 'image/jpg', 'image/jpeg', 'image/gif');
-            $upload->savePath = $upload_dir;
-            $upload->saveRule = 'uniqid';
-            if ($upload->upload()) {
-                $uploadList = $upload->getUploadFileInfo();
-                $title = $rand_num . ',' . $uploadList[0]['savename'];
-                exit(json_encode(array('error' => 0, 'url' => $this->config['site_url'] . '/upload/images/' . $rand_num . '/' . $uploadList[0]['savename'], 'title' => $title)));
-            } else {
-                exit(json_encode(array('error' => 1, 'message' => $upload->getErrorMsg())));
-            }
-        } else {
-            exit(json_encode(array('error' => 1, 'message' => '没有选择图片')));
-        }
-    }
+	public function ajax_upload_pic()
+	{
+		if($_FILES['imgFile']['error'] != 4) {
+			$img_admin_id = sprintf("%09d", $this->system_session['id']);
+			$rand_num = substr($img_admin_id, 0, 3) . '/' . substr($img_admin_id, 3, 3) . '/' . substr($img_admin_id, 6, 3);
+			$upload_dir = "./upload/images/{$rand_num}/";
+			if(!is_dir($upload_dir)) {
+				mkdir($upload_dir, 0777, true);
+			}
+			import('ORG.Net.UploadFile');
+			$upload = new UploadFile();
+			$upload->maxSize = 3 * 1024 * 1024;
+			$upload->allowExts = array('jpg', 'jpeg', 'png', 'gif');
+			$upload->allowTypes = array('image/png', 'image/jpg', 'image/jpeg', 'image/gif');
+			$upload->savePath = $upload_dir;
+			$upload->saveRule = 'uniqid';
+			if($upload->upload()) {
+				$uploadList = $upload->getUploadFileInfo();
+				$title = $rand_num . ',' . $uploadList[0]['savename'];
+				exit(json_encode(array('error'                            => 0, 'url' => $this->config['site_url'] .
+					'/upload/images/' .
+					$rand_num . '/' . $uploadList[0]['savename'], 'title' => $title)));
+			}
+			else {
+				exit(json_encode(array('error' => 1, 'message' => $upload->getErrorMsg())));
+			}
+		}
+		else {
+			exit(json_encode(array('error' => 1, 'message' => '没有选择图片')));
+		}
+	}
 
-    public function ajax_upload_file()
-    {
-        if (empty($_GET['name'])) {
-            exit(json_encode(array('error' => 1, 'message' => '不知道您要上传到哪个配置项，请重试。')));
-        }
-        $now_config = D('Config')->field('`name`,`type`')->where(array('name' => $_GET['name']))->find();
-        if (empty($now_config)) {
-            exit(json_encode(array('error' => 1, 'message' => '您正在上传的配置项不存在，请重试。')));
-        }
-        $tmp_type_arr = explode('&', $now_config['type']);
-        $type_arr = array();
-        foreach ($tmp_type_arr as $k => $v) {
-            $tmp_value = explode('=', $v);
-            $type_arr[$tmp_value[0]] = $tmp_value[1];
-        }
-        $allowExts = array_key_exists('file', $type_arr) ? explode(',', $type_arr['file']) : array();
-        if ($_FILES['imgFile']['error'] != 4) {
-            $img_admin_id = sprintf("%09d", $this->system_session['id']);
-            $rand_num = substr($img_admin_id, 0, 3) . '/' . substr($img_admin_id, 3, 3) . '/' . substr($img_admin_id, 6, 3);
-            $upload_dir = "./upload/files/{$rand_num}/";
-            if (!is_dir($upload_dir)) {
-                mkdir($upload_dir, 0777, true);
-            }
-            import('ORG.Net.UploadFile');
-            $upload = new UploadFile();
-            $upload->maxSize = 10 * 1024 * 1024;
-            $upload->allowExts = $allowExts;
-            $upload->savePath = $upload_dir;
-            $upload->saveRule = 'uniqid';
-            if ($upload->upload()) {
-                $uploadList = $upload->getUploadFileInfo();
-                $title = $rand_num . ',' . $uploadList[0]['savename'];
-                exit(json_encode(array('error' => 0, 'url' => './upload/files/' . $rand_num . '/' . $uploadList[0]['savename'], 'title' => $title)));
-            } else {
-                exit(json_encode(array('error' => 1, 'message' => $upload->getErrorMsg())));
-            }
-        } else {
-            exit(json_encode(array('error' => 1, 'message' => '没有选择文件')));
-        }
-    }
+	public function ajax_upload_file()
+	{
+		if(empty($_GET['name'])) {
+			exit(json_encode(array('error' => 1, 'message' => '不知道您要上传到哪个配置项，请重试。')));
+		}
+		$now_config = D('Config')->field('`name`,`type`')->where(array('name' => $_GET['name']))->find();
+		if(empty($now_config)) {
+			exit(json_encode(array('error' => 1, 'message' => '您正在上传的配置项不存在，请重试。')));
+		}
+		$tmp_type_arr = explode('&', $now_config['type']);
+		$type_arr = array();
+		foreach ($tmp_type_arr as $k => $v) {
+			$tmp_value = explode('=', $v);
+			$type_arr[$tmp_value[0]] = $tmp_value[1];
+		}
+		$allowExts = array_key_exists('file', $type_arr) ? explode(',', $type_arr['file']) : array();
+		if($_FILES['imgFile']['error'] != 4) {
+			$img_admin_id = sprintf("%09d", $this->system_session['id']);
+			$rand_num =
+				substr($img_admin_id, 0, 3) . '/' . substr($img_admin_id, 3, 3) . '/' . substr($img_admin_id, 6, 3);
+			$upload_dir = "./upload/files/{$rand_num}/";
+			if(!is_dir($upload_dir)) {
+				mkdir($upload_dir, 0777, true);
+			}
+			import('ORG.Net.UploadFile');
+			$upload = new UploadFile();
+			$upload->maxSize = 10 * 1024 * 1024;
+			$upload->allowExts = $allowExts;
+			$upload->savePath = $upload_dir;
+			$upload->saveRule = 'uniqid';
+			if($upload->upload()) {
+				$uploadList = $upload->getUploadFileInfo();
+				$title = $rand_num . ',' . $uploadList[0]['savename'];
+				exit(json_encode(array('error' => 0,
+				                       'url'   => './upload/files/' . $rand_num . '/' . $uploadList[0]['savename'],
+				                       'title' => $title)));
+			}
+			else {
+				exit(json_encode(array('error' => 1, 'message' => $upload->getErrorMsg())));
+			}
+		}
+		else {
+			exit(json_encode(array('error' => 1, 'message' => '没有选择文件')));
+		}
+	}
 
-    public function im()
-    {
-        if (empty($this->config['site_url'])) {
-            exit(json_encode(array('error_code' => true, 'msg' => '先填写您网站的域名')));
-        }
-        if (empty($this->config['wechat_appid']) || empty($this->config['wechat_appsecret'])) {
-            exit(json_encode(array('error_code' => true, 'msg' => '先设置站点的微信公众号信息')));
-        }
+	public function group()
+	{
+		$list = D('Config_group')->order('`sort` ASC,`id` ASC')->select();
+		$this->assign('list', $list);
+		$this->display();
+	}
 
-        $array = parse_url($this->config['site_url']);
-        $host = $array['host'];
-        import('ORG.Net.Http');
-        $http = new Http();
-        $data = array(
-            'domain' => $host,
-            'label' => '',
-            'from' => '3',
-            'wx_app_id' => $this->config['wechat_appid'],
-            'wx_app_secret' => $this->config['wechat_appsecret'],
-            'activity_url' => $this->config['site_url'] . '/admin.php?c=Imapi&a=activity',        //活动链接
-            'my_url' => $this->config['site_url'] . '/admin.php?c=Imapi&a=my',        //活动链接
-            'msg_tip_url' => $this->config['site_url'] . '/admin.php?c=Imapi&a=index',        //消息提醒链接
-        );
+	public function group_edit()
+	{
+		$db = D('Config_group');
+		if(IS_POST) {
+			$data['id'] = $this->_post('id', 'trim,intval');
+			$data['name'] = $this->_post('name');
+			if(empty($data['name']))
+				$this->frame_submit_tips(0, '请填写分组名称！');
 
+			$data['action'] = $this->_post('action');
+			$data['status'] = $this->_post('status');
+			if($data['id']) {
+				if($db->where(array('id' => $data['id']))->save($data)) {
+					$this->frame_submit_tips(1, '修改成功！');
+				}
+				else {
+					$this->frame_submit_tips(0, '修改失败！请重试~');
+				}
+			}
+			else {
+				if($data['id'] = $db->add($data)) {
+					$this->frame_submit_tips(1, '添加成功！');
+				}
+				else {
+					$this->frame_submit_tips(0, '添加失败！请重试~');
+				}
+			}
+		}
 
-        $return = Http::curlPost('http://im-link.meihua.com/api/app_create.php', $data);
-        $return = json_decode($return, true);
-        if ($return['err_code']) {
-            exit(json_encode(array('error_code' => true, 'msg' => $return['err_msg'])));
-        } else {
-            if (D('Config')->where("`name`='im_appid'")->find()) {
-                D('Config')->where("`name`='im_appid'")->save(array('value' => $return['app_id']));
-            } else {
-                D('Config')->add(array('name' => 'im_appid', 'value' => $return['app_id'], 'gid' => 0, 'status' => 1));
-            }
-            if (D('Config')->where("`name`='im_appkey'")->find()) {
-                D('Config')->where("`name`='im_appkey'")->save(array('value' => $return['app_key']));
-            } else {
-                D('Config')->add(array('name' => 'im_appkey', 'value' => $return['app_key'], 'gid' => 0, 'status' => 1));
-            }
-            S(C('now_city') . 'config', null);
-            exit(json_encode(array('error_code' => false, 'msg' => '获取成功')));
-        }
-    }
+		$gid = I('get.id', 0, 'trim,intval');
+		$group = array('sort' => 0, 'status' => 1);
+		if($gid) {
+			$group = $db->where(array('id' => $gid))->find();
+			if(empty($group)) {
+				$this->frame_submit_tips(0, '记录不存在或已被删除！');
+			}
+		}
+		$this->assign('group', $group);
+
+		$this->display();
+	}
+
+	public function group_del()
+	{
+		$menu_id = $this->_get('id', 'trim,intval');
+		if(!$menu_id)
+			$this->error('参数错误！');
+
+		D('Config_group')->where(array('id' => $menu_id))->delete();
+		$this->success('删除成功！');
+	}
+
+	public function config()
+	{
+		$gid = I('get.group');
+		if(!$gid)
+			$this->error_tips('参数错误！');
+
+		$group = D('Config_group')->where(array('id' => $gid))->find();
+		if(empty($group))
+			$this->error_tips('该分组不存在！');
+
+		$this->assign('group', $group);
+
+		$list = D('Config')->where(array('gid' => $gid))->select();
+		$this->assign('list', $list);
+
+		$this->display();
+	}
+
+	public function config_edit()
+	{
+		$db = D('Config');
+		if(IS_POST) {
+			// SELECT `id`, `name`, `type`, `value`, `info`, `desc`, `tab_id`, `tab_name`, `gid`, `sort`, `status` FROM `tp_config` WHERE 1
+			$data['id'] = $this->_post('id', 'trim,intval');
+			$data['gid'] = $this->_post('gid', 'trim,intval');
+			if(!$data['gid'])
+				$this->frame_submit_tips(0, '请选择配置分组！');
+
+			$data['info'] = $this->_post('info');
+			$data['name'] = $this->_post('name');
+			if(empty($data['info']) || empty($data['name']))
+				$this->frame_submit_tips(0, '配置ID和名称不能为空！');
+
+			$data['type'] = $this->_post('type');
+			$data['value'] = $this->_post('value');
+			if(empty($data['type']) || empty($data['value']))
+				$this->frame_submit_tips(0, '配置类型和值不能为空！');
+			$data['desc'] = $this->_post('desc', 'trim');
+
+			$data['tab_id'] = $this->_post('tab_id', 'trim');
+			$data['tab_name'] = $this->_post('tab_name', 'trim');
+
+			$data['status'] = $this->_post('status', 'trim,intval');
+			$data['sort'] = $this->_post('sort', 'trim,intval');
+			if($data['id']) {
+				if($db->where(array('id' => $data['id']))->save($data)) {
+					$this->frame_submit_tips(1, '修改成功！');
+				}
+				else {
+					$this->frame_submit_tips(0, '修改失败！请重试~');
+				}
+			}
+			else {
+				if($data['id'] = $db->add($data)) {
+					$this->frame_submit_tips(1, '添加成功！');
+				}
+				else {
+					$this->frame_submit_tips(0, '添加失败！请重试~');
+				}
+			}
+		}
+		$gid = I('get.group', 0, 'trim,intval');
+		$config_id = I('get.id', 0, 'trim,intval');
+		$cfg = array('gid' => $gid, 'sort' => 0, 'status' => 1);
+		if($config_id) {
+			$cfg = $db->where(array('id' => $config_id))->find();
+			if(empty($cfg)) {
+				$this->frame_submit_tips(0, '记录不存在或已被删除！');
+			}
+		}
+		$this->assign('cfg', $cfg);
+
+		$this->assign('groups', D('Config_group')->order('`sort` ASC,`id` DESC')->select());
+
+		$this->display();
+	}
+
+	public function config_del()
+	{
+		$menu_id = $this->_get('id', 'trim,intval');
+		if(!$menu_id)
+			$this->error('参数错误！');
+
+		D('Config')->where(array('id' => $menu_id))->delete();
+		$this->success('删除成功！');
+	}
+
+	private function menu_tree($list, $parent_id, $str = '')
+	{
+		$data = array();
+		foreach ($list as $item) {
+			if($item['fid'] != $parent_id)
+				continue;
+
+			$item['prefix'] = $str;
+
+			$data[] = $item;
+
+			$children = $this->menu_tree($list, $item['id'], $str . '|——');
+			if(!empty($children))
+				$data = array_merge($data, $children);
+		}
+
+		return $data;
+	}
+
+	public function menu()
+	{
+		$list = D('System_menu')->order('`sort` ASC, `id` DESC')->select();
+		$this->assign('list', $this->menu_tree($list, 0, ''));
+		$this->display();
+	}
+
+	public function menu_edit()
+	{
+		$db = D('System_menu');
+		if(IS_POST) {
+			$data['id'] = $this->_post('id', 'trim,intval');
+			$data['fid'] = $this->_post('fid', 'trim,intval');
+			$data['name'] = $this->_post('name');
+			if(empty($data['name']))
+				$this->frame_submit_tips(0, '请填写菜单名称！');
+
+			$data['module'] = $this->_post('module');
+			$data['action'] = $this->_post('action');
+			$data['status'] = $this->_post('status', 'trim,intval');
+			$data['sort'] = $this->_post('sort', 'trim,intval');
+			if($data['id']) {
+				if($db->where(array('id' => $data['id']))->save($data)) {
+					$this->frame_submit_tips(1, '修改成功！');
+				}
+				else {
+					$this->frame_submit_tips(0, '修改失败！请重试~');
+				}
+			}
+			else {
+				if($data['id'] = $db->add($data)) {
+					$this->frame_submit_tips(1, '添加成功！');
+				}
+				else {
+					$this->frame_submit_tips(0, '添加失败！请重试~');
+				}
+			}
+		}
+
+		$parents = $db->where(array('fid' => 0))->order('`sort` ASC, `id` DESC')->select();
+		$this->assign('parents', $parents);
+
+		$menu = array('sort' => 0, 'status' => 1);
+		$id = $this->_get('id', 'trim,intval');
+		if($id) {
+			$menu = $db->where(array('id' => $id))->find();
+			if(empty($menu))
+				$this->frame_submit_tips(0, '记录不存在或已被删除！');
+		}
+		$this->assign('menu', $menu);
+
+		$this->display();
+	}
+
+	public function menu_del()
+	{
+		$menu_id = $this->_get('id', 'trim,intval');
+		if(!$menu_id)
+			$this->error('参数错误！');
+
+		D('System_menu')->where(array('id' => $menu_id))->delete();
+		$this->success('删除成功！');
+	}
+
+	//修改分类状态
+	public function menu_status()
+	{
+		$db = M('System_menu');
+		$id = $this->_post('id', 'trim,intval');
+		$status = $this->_post('status', 'trim,intval');
+		$db->where(array('id' => $id))->save(array('status' => $status));
+	}
+
+//	public function im()
+//	{
+//		if(empty($this->config['site_url'])) {
+//			exit(json_encode(array('error_code' => true, 'msg' => '先填写您网站的域名')));
+//		}
+//		if(empty($this->config['wechat_appid']) || empty($this->config['wechat_appsecret'])) {
+//			exit(json_encode(array('error_code' => true, 'msg' => '先设置站点的微信公众号信息')));
+//		}
+//
+//		$array = parse_url($this->config['site_url']);
+//		$host = $array['host'];
+//		import('ORG.Net.Http');
+//		$http = new Http();
+//		$data = array(
+//			'domain'        => $host,
+//			'label'         => '',
+//			'from'          => '3',
+//			'wx_app_id'     => $this->config['wechat_appid'],
+//			'wx_app_secret' => $this->config['wechat_appsecret'],
+//			'activity_url'  => $this->config['site_url'] . '/admin.php?c=Imapi&a=activity',        //活动链接
+//			'my_url'        => $this->config['site_url'] . '/admin.php?c=Imapi&a=my',        //活动链接
+//			'msg_tip_url'   => $this->config['site_url'] . '/admin.php?c=Imapi&a=index',        //消息提醒链接
+//		);
+//
+//
+//		$return = Http::curlPost('http://im-link.meihua.com/api/app_create.php', $data);
+//		$return = json_decode($return, true);
+//		if($return['err_code']) {
+//			exit(json_encode(array('error_code' => true, 'msg' => $return['err_msg'])));
+//		}
+//		else {
+//			if(D('Config')->where("`name`='im_appid'")->find()) {
+//				D('Config')->where("`name`='im_appid'")->save(array('value' => $return['app_id']));
+//			}
+//			else {
+//				D('Config')->add(array('name' => 'im_appid', 'value' => $return['app_id'], 'gid' => 0, 'status' => 1));
+//			}
+//			if(D('Config')->where("`name`='im_appkey'")->find()) {
+//				D('Config')->where("`name`='im_appkey'")->save(array('value' => $return['app_key']));
+//			}
+//			else {
+//				D('Config')->add(array('name'   => 'im_appkey', 'value' => $return['app_key'], 'gid' => 0,
+//				                       'status' => 1));
+//			}
+//			S(C('now_city') . 'config', null);
+//			exit(json_encode(array('error_code' => false, 'msg' => '获取成功')));
+//		}
+//	}
 }
