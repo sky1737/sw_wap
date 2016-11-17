f6a0cc10e3b6f5e9f4b2a4c4d3c47062537718f5
谭康 <tkaven@vip.qq.com>
2016-11-17 17:14:30 +0800

    fix

--- template/wap/default/supplier_footer.php
+++ template/wap/default/supplier_footer.php
@@ -5,14 +5,15 @@
 			<div class="nav-b5-relative"> <a href="./"><i class="icon-nav-bag"></i>商城</a></div>
 			<!--home.php?id=<?php echo $now_store['store_id']; ?>--> 
 		</dd>
-		<dd<?php //echo strpos($_SERVER['REQUEST_URI'], 'drp_ucenter') ? ' class="active"' : ''; ?>>
-			<div class="nav-b5-relative"> <a href="./supplier_ucenter.php"><i class="icon-nav-cart"></i>供应</a> </div>
-		</dd>
 		<dd<?php //echo strpos($_SERVER['REQUEST_URI'], 'drp_store') ? ' class="active"' : ''; ?>>
-			<div class="nav-b5-relative"> <a href="./qrcode.php"><i class="icon-nav-store"></i>推广</a> </div>
+			<div class="nav-b5-relative"> <a href="./category.php"><i class="icon-nav-store"></i>分类</a> </div>
 		</dd>
 		<dd<?php //echo strpos($_SERVER['REQUEST_URI'], 'drp_ucenter.php?a=personal') ? ' class="active"' : ''; ?>>
-			<div class="nav-b5-relative"> <a href="./my.php"><i class="icon-nav-heart"></i>会员</a> </div>
+			<div class="nav-b5-relative"> <a href="./cart.php"><i class="icon-nav-cart"></i>购物车</a> </div>
+		</dd>
+
+		<dd<?php //echo strpos($_SERVER['REQUEST_URI'], 'drp_ucenter') ? ' class="active"' : ''; ?>>
+			<div class="nav-b5-relative"> <a href="./supplier_ucenter.php"><i class="icon-nav-heart"></i>供应商</a> </div>
 		</dd>
 		<!--<dd>
 		<div class="nav-b5-relative">
