a9547141e785796052eb8ad3064bcc8b9a982209
dev <dev@waphe.com>
2016-11-28 18:02:04 +0800

    界面调整。

--- template/wap/default/drp_payfor_no.php
+++ template/wap/default/drp_payfor_no.php
@@ -1,20 +1,37 @@
-<?php if(!defined('TWIKER_PATH')) exit('deny access!'); ?>
+<?php if (!defined('TWIKER_PATH')) exit('deny access!'); ?>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no" />
-	<meta name="apple-mobile-web-app-capable" content="yes" />
-	<meta name="apple-mobile-web-app-status-bar-style" content="black" />
-	<meta name="format-detection" content="telephone=no" />
-	<title>请先获取推广码 - <?php echo option('config.site_name'); ?></title>
-	<link rel="stylesheet" type="text/css" href="<?php echo TPL_URL; ?>css/foundation.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo TPL_URL; ?>css/normalize.css">
-	<link rel="stylesheet" type="text/css" href="<?php echo TPL_URL; ?>css/common.css">
-	<script src="<?php echo TPL_URL; ?>js/jquery.js"></script>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="initial-scale=1.0,maximum-scale=1.0,user-scalable=no"/>
+    <meta name="apple-mobile-web-app-capable" content="yes"/>
+    <meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+    <meta name="format-detection" content="telephone=no"/>
+    <title>请先获取推广码 - <?php echo option('config.site_name'); ?></title>
+    <!--<link rel="stylesheet" type="text/css" href="--><?php //echo TPL_URL; ?><!--css/foundation.css">-->
+    <!--<link rel="stylesheet" type="text/css" href="--><?php //echo TPL_URL; ?><!--css/normalize.css">-->
+    <!--<link rel="stylesheet" type="text/css" href="--><?php //echo TPL_URL; ?><!--css/common.css">-->
+    <!--<script src="--><?php //echo TPL_URL; ?><!--js/jquery.js"></script>-->
+    <style type="text/css">
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            max-width: 960px;
+            margin: 0 auto;
+        }
+
+        a img {
+            border: 0;
+            display: block;
+            width: 100%;
+        }
+    </style>
 </head>
 
-<body style="max-width:960px;margin: 0 auto;">
-<div><img src="<?php echo TPL_URL; ?>images/no_parent.jpg" style="width: 100%;" alt="" /></div>
+<body>
+<div><a href="./"><img src="<?php echo TPL_URL; ?>images/no_parent.jpg" style="width: 100%;" alt=""/></a></div>
 <!--<div style="padding: 100px 30px;line-height:2em;font-size:1.5em;">-->
 <!--	请先获取推广码！<br/>扫码成为--><?php //echo option('config.site_name'); ?><!--的代理！-->
 <!--</div>-->
