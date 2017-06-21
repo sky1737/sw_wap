065f8cb05dcda8bd94c8ccbc408547913446a5a7
yws_cn <yws_cn@sinan.com>
2017-06-21 08:48:37 +0800

    gitignore

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * 配置文件
+ */
+if(!defined('TWIKER_PATH')) exit('deny access!');
+
+return array(
+	'DB_HOST'=>'localhost',
+	'DB_NAME'=>'yws_sw',//数据库名
+	'DB_USER'=>'root',//数据库用户名
+	'DB_PWD'=>'Yws123456AdminMysql',//数据库密码
+	'DB_PORT'=>'3306',
+	'DB_PREFIX'=>'tp_',
+	'DB_CHARSET'=>'utf8',
+);
+
+//return array(
+//	'DB_HOST'=>'127.0.0.1',
+//	'DB_NAME'=>'yunws',//数据库名
+//	'DB_USER'=>'root',//数据库用户名
+//	'DB_PWD'=>'root',//数据库密码
+//	'DB_PORT'=>'3306',
+//	'DB_PREFIX'=>'tp_',
+//	'DB_CHARSET'=>'utf8',
+//);
